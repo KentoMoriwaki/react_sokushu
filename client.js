92e39203df5deb79a88da8c85ea2106fc4ac9eed
Kento Moriwaki <kento@wantedly.com>
2016-01-30 17:28:42 +0900

    Move to components

--- client.js
+++ client.js
@@ -1,29 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { introduction: 'My name is Kento' }
-  }
-  handleSubmit(e) {
-    e.preventDefault()
-    alert(this.state.introduction)
-  }
-  onChange(e) {
-    this.setState({introduction: e.target.value})
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          <textarea value={this.state.introduction} onChange={(e) => this.onChange(e)}></textarea>
-          <input type="submit" value="Save" />
-        </form>
-      </div>
-    )
-  }
-}
+import App from './components/App'
 
 render(<App />, document.getElementById("root"))
---
+++ components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { introduction: 'My name is Kento' }
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    alert(this.state.introduction)
+  }
+  onChange(e) {
+    this.setState({introduction: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <textarea value={this.state.introduction} onChange={(e) => this.onChange(e)}></textarea>
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+    )
+  }
+}
