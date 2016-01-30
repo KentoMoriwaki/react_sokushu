40fcdf879147ab7e685c2420416c13a5d8d4fb02
Kento Moriwaki <kento@wantedly.com>
2016-01-30 16:46:07 +0900

    Basic react component

--- client.js
+++ client.js
@@ -1,1 +1,29 @@
-alert("Hello world");
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { introduction: 'My name is Kento' }
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    alert(this.state.introduction)
+  }
+  onChange(e) {
+    this.setState({introduction: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <textarea value={this.state.introduction} onChange={(e) => this.onChange(e)}></textarea>
+          <input type="submit" value="Save" />
+        </form>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById("root"))
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
     path: './static/dist',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
 }
