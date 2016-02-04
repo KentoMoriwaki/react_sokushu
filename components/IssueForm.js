47bd6cc3db05be94850b78fae8f7e9d5c587c547
Kento Moriwaki <kento@wantedly.com>
2016-02-04 15:20:40 +0900

    Add style

--- components/IssueForm.js
+++ components/IssueForm.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import Radium from 'radium'
+import color from 'color'
 
-export default class IssueForm extends Component {
+class IssueForm extends Component {
   constructor(props) {
     super(props)
     this.state = this.createEmptyState()
@@ -30,12 +32,37 @@ export default class IssueForm extends Component {
         <form onSubmit={this.onSubmit.bind(this)}>
           <fieldset>
             <legend>Create Issue</legend>
-            <input type="text" value={this.state.title} onChange={this.onTitleChange.bind(this)} placeholder="Input title" />
-            <textarea value={this.state.description} onChange={this.onDescriptionChange.bind(this)} placeholder="Input description" />
-            <button>Save</button>
+            <input style={[styles.input]} type="text" value={this.state.title} onChange={this.onTitleChange.bind(this)} placeholder="Input title" />
+            <textarea style={[styles.input, styles.textarea]} value={this.state.description} onChange={this.onDescriptionChange.bind(this)} placeholder="Input description" />
+            <button style={[styles.button]}>Save</button>
           </fieldset>
         </form>
       </div>
     )
   }
 }
+
+const styles = {
+  input: {
+    display: 'block',
+    width: '200px',
+    padding: '6px 10px',
+    margin: '10px 0',
+    border: '1px solid #ccc'
+  },
+  textarea: {
+    height: '100px'
+  },
+  button: {
+    backgroundColor: '#00A4BB',
+    border: 'none',
+    padding: '6px 15px',
+    color: 'white',
+    cursor: 'pointer',
+    ':hover': {
+      backgroundColor: color('#00A4BB').darken(0.2).hexString(),
+    }
+  }
+}
+
+export default Radium(IssueForm)
